8eab3ad9fb7e455887d124e58ef1a57d20352aa8
Noah Lavayen <noah.lavayen2@gmail.com>
2026-05-28 13:19:38 -0400

    fix broken imports in interactionCreate

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,8 +1,6 @@
 import { Events, MessageFlags } from "discord.js";
 import { logger } from "../utils/logger.js";
 import { getGuildConfig } from "../services/guildConfig.js";
-import { handleApplicationModal } from "../commands/Community/apply.js";
-import { handleApplicationReviewModal } from "../commands/Community/app-admin.js";
 import {
   handleInteractionError,
   createError,
